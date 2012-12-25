216eed23527d8d1629f185f221e1c15c09757af2
Zach Stevenson <zachs1590@gmail.com>
2012-12-24 22:00:08 -0500

    Added my comments about this module

--- jsdump/jsdump.js
+++ jsdump/jsdump.js
@@ -1,24 +1,102 @@
-exports.writeDump = writeDump;
-exports.dump = dump; 
+/***
+  ** Copyright Zach Stevenson 2012
+  **
+  ** This module was created to emulate the dump functionality in Adobe Coldfusion
+  ** It will allow you to visually see what type of objects you have created making
+  ** the development process easier and faster.
+  **
+  ** I tend to use a closure inside of my createServer to alias the writeDump
+  ** function that automatically passes the response to it so i don't have to
+  ** constantly write it over and over again. I will have an example after this.
+  ** 
+  ** This module is being hosted at github at https://github.com/zachs1590/nodeModules/
+  **
+  ** e:  zachs1590@gmail.com
+  ** t:  cfzach              //Ironic, no?
+  ** f:  zachs1590
+  **
+  ***/
 
+
+/* -------- EXAMPLE OF CREATE SERVER CLOSURE -------- */
+/*
+	var dump = require('./modules/dump');
+
+	http.createServer(function(request, response)
+	{
+		var writeDump = function(variable, top)
+		{
+			return dump.writeDump(response, variable, top);
+		}
+
+		writeDump(request);
+	});
+
+*/
+
+/* -------- EXPORTS -------- */
+exports.writeDump = writeDump;
+exports.dump      = dump; 
+
+
+/* -------- CONSTANTS -------- */
 var undefinedDataTypes = {'undefined':1};
 var simpleDataTypes = {'string':1, 'number':1, 'boolean':1};
 var arrayTypes = {'array':1};
 var objectTypes = {'object':1};
+var _DEFAULT_TOP = 9999;
 
 
+/* -------- PUBLIC FUNCTIONS -------- */
+
+/***
+  ** public void function writeDump(response, variable[, top = 9999])
+  **
+  ** writeDump is intended to be a nice utility function for the dump function
+  ** It's sole purpose is to immediately write what is dumped to page.
+  **
+  ** ARGUMENTS:
+  ** response - REQUIRED - Type: Class - http.ServerResponse
+  ** 		This is the response object you get when you createServer on http
+  ** variable - REQUIRED - Type: Any
+  ** 		This is any type of object that you want to have dumped to the screen
+  ** top - NOT REQUIRED -Type: numeric
+  ** 		This is the amount of levels deep a dump can go.  This is very important
+  **		when you have objects that may contain references of themselves or when
+  **		you have extremely large amounts of data nested. This count is 1 indexed,
+  **		not 0, so the first depth is 1.
+  **
+  ***/
 function writeDump(response, variable, top)
 {
 	response.write(dump(variable, top));
 }
 
+/***
+  ** public string function dump(variable[, top = 9999])
+  **
+  ** dump is the main function of this module. It kicks off this entire mumbo jumbo
+  ** This function will start the origin string and default values where necessary
+  ** to have a good output.  This function passes off a lot of the heavy lifting to
+  ** the private render function.
+  **
+  ** ARGUMENTS:
+  ** variable - REQUIRED - Type: Any
+  ** 		This is any type of object that you want to have dumped to the screen
+  ** top - NOT REQUIRED -Type: numeric
+  ** 		This is the amount of levels deep a dump can go.  This is very important
+  **		when you have objects that may contain references of themselves or when
+  **		you have extremely large amounts of data nested. This count is 1 indexed,
+  **		not 0, so the first depth is 1.
+  **
+  ***/
 function dump(variable, top)
 {
 	var output = "";
 
 	if(typeof top == 'undefined' || !isFinite(top))
 	{
-		top = 9999;
+		top = _DEFAULT_TOP;
 	}
 
 	output += getCSS();
@@ -27,7 +105,13 @@ function dump(variable, top)
 	output += '</div>';
 	output += '<br />';
 	return output;
-};
+}
+
+
+
+
+
+/* -------- PRIVATE FUNCTIONS -------- */
 
 function render(output, variable, top, currentLevel)
 {
@@ -132,5 +216,3 @@ function getCSS()
 {
 	return '<style type="text/css">.DUMP_MODULE *{font-family:helvetica,arial,sans-serif;font-size:12px}.DUMP_MODULE{background-color:#fff}.DUMP_MODULE p{margin:0 5px;color:#111}.DUMP_MODULE table{border-spacing:0;border-collapse:collapse}.DUMP_MODULE td{padding:2px 0}.DUMP_MODULE .DUMP_MODULE__TYPE{font-weight:bold;text-transform:capitalize}.DUMP_MODULE .DUMP_MODULE__UNDEFINED p{font-family:georgia,times,serif;font-size:14px;font-style:italic;margin-bottom:10px;background-color:#fff}.DUMP_MODULE .DUMP_MODULE__SIMPLE .DUMP_MODULE__SIMPLE_ROW{display:block;border:2px solid #888}.DUMP_MODULE .DUMP_MODULE__SIMPLE .DUMP_MODULE__SIMPLE_TYPE{background-color:#eee;border-right:1px solid #888}.DUMP_MODULE .DUMP_MODULE__ARRAY .DUMP_MODULE__ARRAY_ROW{display:block;border:2px solid #006d00;border-top:0}.DUMP_MODULE .DUMP_MODULE__ARRAY .DUMP_MODULE__ARRAY_ROW:first-child{border-top:2px solid #006d00;background-color:#00a300;color:#fff}.DUMP_MODULE .DUMP_MODULE__ARRAY .DUMP_MODULE__ARRAY_INDEX{background-color:#cefecc;border-right:2px solid #006d00}.DUMP_MODULE .DUMP_MODULE__ARRAY .DUMP_MODULE__ARRAY_TYPE,.DUMP_MODULE .DUMP_MODULE__OBJECT .DUMP_MODULE__OBJECT_TYPE{padding:2px}.DUMP_MODULE .DUMP_MODULE__ARRAY .DUMP_MODULE__ARRAY_VALUE,.DUMP_MODULE .DUMP_MODULE__OBJECT .DUMP_MODULE__OBJECT_VALUE{padding:3px}.DUMP_MODULE .DUMP_MODULE__OBJECT .DUMP_MODULE__OBJECT_ROW{display:block;border:2px solid #0100c8;border-top:0}.DUMP_MODULE .DUMP_MODULE__OBJECT .DUMP_MODULE__OBJECT_ROW:first-child{border-top:2px solid #0100c8;background-color:#4644d4;color:#fff}.DUMP_MODULE .DUMP_MODULE__OBJECT .DUMP_MODULE__OBJECT_INDEX{background-color:#cedcff;border-right:2px solid #0100c8;text-transform:none}</style>';
 }
-
-
